51340eadbc171d622e98389c5e821c83cd927e32
pelin12 <58700817+pelin12@users.noreply.github.com>
2025-03-09 21:39:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 def int_to_roman(num):
+    roman_map = [
+           (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
+    
+    roman_string = ""
+    for val, rom in roman_map:
+        while num >= val:
+            roman_string += rom
+            num -= val
+    
+    return roman_string
     """
     Convert an integer to a Roman numeral.
 
